763b44df318f71af31a237590fe8b729f1786916
Loisel CHRISTOPHER <lchristo@e3r9p5.42.fr>
2020-07-22 06:21:50 +0200

    distance resolution ecrant

--- src/ft_screen.c
+++ src/ft_screen.c
@@ -6,7 +6,7 @@
 /*   By: lchristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 04:57:07 by lchristo          #+#    #+#             */
-/*   Updated: 2020/07/21 04:18:15 by lchristo         ###   ########.fr       */
+/*   Updated: 2020/07/22 06:21:21 by lchristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,42 @@
 
 void		ft_check_wall(t_t *t)
 {
-	int 	y;
-	int 	x;
+	float 	y;
+	float 	ya;
+	float 	x;
+	float 	x1;
+	float 	x2;
 	float	ray;
+	float	tempo;
 
+	ya = 1;
 	ray = 0;
-	y = t->y;
-	x = t->x;
-	t->rot = 20;
-	printf("[%d][%d] ->[%f][%f] %c\n", y, x, t->yray, t->xray, t->tb[y][x]);
-	while (y < t->maxy && x < t->maxx && t->tb[y][x] != '1')
-	{
-		ray = ray + 0.01;
-		t->yray = (ray * cos(t->rot));
-		t->xray = (ray * cos(90 - t->rot));
-		y = y + (int) t->yray;
-		x = x + (int) t->xray;
-	}
-	printf("[%d][%d] ->[%f][%f] %c\n", y, x, t->yray, t->xray, t->tb[y][x]);
+	x1 = 0;
+	x2 = 0;
+	y = t->y + 0.5;
+	x = t->x + 0.5;
+	printf("rot = %f\n", t->rot);
+	t->rot = t->rot * (M_PI/180);
+	ya = (y - (int)y);
+	if (t->rot < 0)
+		ya = -ya;
+	printf("rot = %f  ya = %f (%f - %d)\n", t->rot, ya, y, (int)y);
+	x1 = ya / tan(t->rot);
+	printf("rot = %f  x1= %f  (%f / %f)\n", t->rot, x1, ya, tan(t->rot));
 }
 
 void		ft_screen(t_t *t)
 {
+	int i;
+
+	i = 0;
 	t->yray = 0;
 	t->xray = 0;
 	ft_check_wall(t);
+	i = 0;
+	while (i < t->maxy)
+	{
+		printf("->%s\n", t->tb[i]);
+		i++;
+	}
 }
